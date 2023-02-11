6001b1b0cdc61b145ab57a17db805883f93f976e
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-02-12 01:05:35 +0200

    -

--- src/conponents/App.tsx
+++ src/conponents/App.tsx
@@ -1,22 +1,11 @@
-import { createGlobalStyle } from 'styled-components';
 import { Route, Routes } from 'react-router-dom';
 import { useEffect, lazy } from 'react';
 import { fetchCours } from '../Redux/operations';
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { Layout } from './Layout/Layout';
 import { Home } from './pages/Home/Home';
+import { GlobalStyle } from '../GlobalStyle';
 
-const GlobalStyle = createGlobalStyle`
-
-  ul,h1,h2,h3,h4,h5,h6,li,p{list-style:none;margin:0;padding:0;};
-  body{
-
-    background: linear-gradient(90deg,#faf0cd, #f1dcd4);
-   color: '#010101'; 
-  }
-a{text-decoration:none;
-color:inherit}
-`;
 const Converter = lazy(() => import('./pages/Converter/Converter'));
 
 const App: React.FC = () => {
