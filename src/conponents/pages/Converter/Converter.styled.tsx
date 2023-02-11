8466e4729a8d8f93c15af6a86034bb75ca7be873
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-02-11 14:53:05 +0200

    added style

--- src/conponents/pages/Converter/Converter.styled.tsx
+++ src/conponents/pages/Converter/Converter.styled.tsx
@@ -1,7 +1,52 @@
 import styled from 'styled-components';
+import { RiExchangeDollarLine } from 'react-icons/ri';
+
 export const ConvertTitle = styled.h2`
   text-align: center;
   font-size: 35px;
   color: #756059;
   margin-bottom: 40px;
+`;
+export const FlagConvert = styled.img`
+  width: 160px;
+  height: 80px;
+  border-radius: 7px;
+  overflow: hidden;
+  margin-right: 10px;
+`;
+export const WrapMainContent = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+`;
+export const WrapSelect = styled.div`
+  display: flex;
+`;
+export const SelectConvert = styled.select`
+  border-top-right-radius: 7px;
+  border-bottom-right-radius: 7px;
+  border: none;
+  display: flex;
+  outline: none;
+  background-color: #e6e6e6;
+  font-size: 25px;
+  color: #756059;
+  font-weight: 600;
+`;
+export const InputConvert = styled.input`
+  border-top-left-radius: 7px;
+  border-bottom-left-radius: 7px;
+  border: none;
+  padding: 5px 10px;
+  font-size: 38px;
+  width: 350px;
+  font-weight: 600;
+  color: #756059;
+`;
+export const ConvertIcon = styled(RiExchangeDollarLine)`
+  height: 50px;
+  width: 50px;
+  color: #c4907e;
 `;
--- src/conponents/pages/Converter/Converter.tsx
+++ src/conponents/pages/Converter/Converter.tsx
@@ -1,7 +1,15 @@
 import { Container } from '../../Container/Container';
 import { Casket } from '../Home/Home.styled';
 import { useAppSelector } from '../../../hooks';
-import { ConvertTitle } from './Converter.styled';
+import {
+  ConvertTitle,
+  FlagConvert,
+  WrapSelect,
+  WrapMainContent,
+  SelectConvert,
+  InputConvert,
+  ConvertIcon,
+} from './Converter.styled';
 import { useState } from 'react';
 import { currencyListWithFlag } from '../../../currencyList';
 
@@ -9,29 +17,29 @@ const Converter: React.FC = () => {
   const { rate, currentRate } = useAppSelector(state => state.exchangeRate);
   const currency = Object.keys(rate);
   // const course = Object.values(rate);
-  const [forCurency, setForCurrency] = useState(currentRate);
+  const [fromCurency, setFromCurrency] = useState(currentRate);
   const [toCurency, setToCurrency] = useState(currentRate);
-  const [forCount, setForCount] = useState('');
-  const [toState, setToState] = useState('');
-  const currentIndexFor = currency.findIndex(el => el === forCurency);
+  const [fromCount, setFromCount] = useState('');
+  const [toCount, setToCount] = useState('');
+  const currentIndexFor = currency.findIndex(el => el === fromCurency);
   const currentIndexTo = currency.findIndex(el => el === toCurency);
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
-    setForCurrency(e.target.value);
-    setToState(((rate[toCurency] / rate[e.target.value]) * +forCount).toFixed(5).toString());
+    setFromCurrency(e.target.value);
+    setToCount(((rate[toCurency] / rate[e.target.value]) * +fromCount).toFixed(5).toString());
   };
   const handleToChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
     setToCurrency(e.target.value);
-    setToState(((rate[e.target.value] / rate[forCurency]) * +forCount).toFixed(5).toString());
+    setToCount(((rate[e.target.value] / rate[fromCurency]) * +fromCount).toFixed(5).toString());
   };
 
   const onForChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setForCount(e.target.value);
-    setToState(((rate[toCurency] / rate[forCurency]) * +e.target.value).toFixed(5).toString());
+    setFromCount(e.target.value);
+    setToCount(((rate[toCurency] / rate[fromCurency]) * +e.target.value).toFixed(5).toString());
   };
   const onToChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setToState(e.target.value);
-    setForCount(((rate[toCurency] / rate[forCurency]) * +e.target.value).toFixed(5).toString());
+    setToCount(e.target.value);
+    setFromCount(((rate[fromCurency] / rate[toCurency]) * +e.target.value).toFixed(5).toString());
   };
 
   return (
@@ -39,32 +47,47 @@ const Converter: React.FC = () => {
       <Container>
         <Casket>
           <ConvertTitle>Convert the required currency</ConvertTitle>
-          {currentIndexFor !== -1 && (
-            <img src={currencyListWithFlag[currentIndexFor][forCurency]} alt="flag" />
-          )}
-          <select onChange={handleChange} value={forCurency}>
-            {currency.map(el => {
-              return (
-                <option key={el} value={el}>
-                  {el}
-                </option>
-              );
-            })}
-          </select>
-          <input type="number" placeholder="0" value={forCount} onChange={onForChange} />
-          <input value={toState} type="number" onChange={onToChange} placeholder="0" />
-          <select onChange={handleToChange} value={toCurency}>
-            {currency.map(el => {
-              return (
-                <option key={el} value={el}>
-                  {el}
-                </option>
-              );
-            })}
-          </select>
-          {currentIndexTo !== -1 && (
-            <img src={currencyListWithFlag[currentIndexTo][toCurency]} alt="flag" />
-          )}
+          <WrapMainContent>
+            <WrapSelect>
+              <p>From</p>
+              {currentIndexFor !== -1 && (
+                <FlagConvert src={currencyListWithFlag[currentIndexFor][fromCurency]} alt="flag" />
+              )}
+
+              <InputConvert
+                type="number"
+                placeholder="0"
+                value={fromCount}
+                onChange={onForChange}
+              />
+              <SelectConvert onChange={handleChange} value={fromCurency}>
+                {currency.map(el => {
+                  return (
+                    <option key={el} value={el}>
+                      {el}
+                    </option>
+                  );
+                })}
+              </SelectConvert>
+            </WrapSelect>
+            <ConvertIcon />
+            <WrapSelect>
+              <p>To</p>
+              {currentIndexTo !== -1 && (
+                <FlagConvert src={currencyListWithFlag[currentIndexTo][toCurency]} alt="flag" />
+              )}
+              <InputConvert value={toCount} type="number" onChange={onToChange} placeholder="0" />{' '}
+              <SelectConvert onChange={handleToChange} value={toCurency}>
+                {currency.map(el => {
+                  return (
+                    <option key={el} value={el}>
+                      {el}
+                    </option>
+                  );
+                })}
+              </SelectConvert>
+            </WrapSelect>
+          </WrapMainContent>
         </Casket>
       </Container>
     </section>
