054e8833a005bdac6669db295c8f33a5b9d9feb8
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-02-13 07:43:52 +0200

    changed files

--- src/globalstyle/GlobalStyle.tsx
+++ src/globalstyle/GlobalStyle.tsx
@@ -1,13 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
 
-  ul,h1,h2,h3,h4,h5,h6,li,p{list-style:none;margin:0;padding:0;};
-  body{
+  ul,h1,h2,h3,h4,h5,h6,li,p{
+    list-style:none;
+    margin:0;
+    padding:0;};
 
+  body{
     background: linear-gradient(90deg,#faf0cd, #f1dcd4);
-   color: '#010101'; 
-  }
-a{text-decoration:none;
-color:inherit};
+    color: '#010101'; 
+  };
+
+  a{
+    text-decoration:none;
+    color:inherit};
 
 `;
