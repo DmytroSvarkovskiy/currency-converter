2ebff72df314006e70c953a8f3239afb6ad610d2
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-02-14 00:14:32 +0200

    -

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './redux';
+import store from './redux/';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
